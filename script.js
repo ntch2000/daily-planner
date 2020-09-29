f2214c6b0f4fb694bc23034d3ff33f8d95eba0f0
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-09-28 21:38:33 -0400

    Added script.js file and linked it to html file

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  console.log("hello world");
+  // DOM VARIABLES
+
+  // JS VARIABLES
+
+  // FUNCTION DEFINITIONS
+
+  // FUNCTION CALLS
+
+  // EVENT LISTENERS
+});
