897ae15fd1ea00602ae7fd64ffcaec28d5063ca8
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-01 15:42:35 -0400

    Initial code to color code time blocks completed. The 9AM slot is colored incorrectly

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   // will need to add past/present/future class based on time of day
   var descriptionDivEl = $("<textarea>").attr(
     "class",
-    "col-11 time-block description past"
+    "col-10 description past"
   );
 
   // targets the paragraph element for the current day and date
@@ -23,13 +23,18 @@ $(document).ready(function () {
     "9AM",
     "10AM",
     "11AM",
-    "12AM",
+    "12PM",
     "1PM",
     "2PM",
     "3PM",
     "4PM",
     "5PM",
   ];
+
+  // grabs the hour of the current time and formats it to the  proper format for comparison to the times in the array
+  var timeHour = moment().format("hA");
+  console.log(timeHour);
+
   // FUNCTION DEFINITIONS
   function populateDate() {
     // sets current day and date
@@ -45,16 +50,33 @@ $(document).ready(function () {
       // potentially move spacing and text location to css file
       var timeDivEl = $("<div>").attr("class", "col-1 hour pt-2 text-right");
 
+      console.log(timeArray[0]);
       // will need to add past/present/future class based on time of day
-      var descriptionDivEl = $("<textarea>").attr(
-        "class",
-        "col-10 description past"
-      );
-
+      // sets the bg color of the current time block to red
+      if (timeArray[i] === timeHour) {
+        var descriptionDivEl = $("<textarea>").attr(
+          "class",
+          "col-10 description present"
+        );
+      } else if (timeArray[i] < timeHour) {
+        var descriptionDivEl = $("<textarea>").attr(
+          "class",
+          "col-10 description past"
+        );
+      } else {
+        // if time is less than current time make past
+        // if time is current time make present
+        // if time is greater than current time make future
+        var descriptionDivEl = $("<textarea>").attr(
+          "class",
+          "col-10 description future"
+        );
+      }
       var saveBtn = $("<button>").attr("class", "saveBtn fas fa-save col-1");
 
       // sets time text to the time slot
-      timeDivEl.text(timeArray[i]);
+      var displayTime = timeArray[i];
+      timeDivEl.text(displayTime);
 
       // adds time slot to row
       rowDivEl.append(timeDivEl);
@@ -72,5 +94,16 @@ $(document).ready(function () {
   // FUNCTION CALLS
   populateDate();
   populateTimeBlocks();
+
+  // var m = moment();
+  // console.log("test moment stuff");
+  // console.log(m.toString());
+  // console.log(m.hour() + " " + m.minute());
+  // console.log(
+  //   moment("2019-06-04 14:00:00").isSame("2019-06-04 14:00:00", "hour")
+  // );
+  // var readable = m.format("hA");
+  // console.log(readable);
+  // console.log(timeArray[3]);
   // EVENT LISTENERS
 });
